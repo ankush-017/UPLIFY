94d149a1dbcc0372e7c8b3b83812062de7ce7451
Ankush kumar <ankushkumar1412005@gmail.com>
2025-06-25 22:04:16 +0530

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import Cors from "cors";
+import cors from "cors";
 import connectDB from './Config/db.js';
 import authRoutes from './Routes/authRoutes.js'
 import { connectRedis } from './Utils/redisClient.js';
@@ -8,16 +8,19 @@ import { connectRedis } from './Utils/redisClient.js';
 dotenv.config();
 
 const app = express();
-app.use(Cors());
+app.use(cors({
+    origin: "https://uplify.onrender.com",
+    credentials: true
+}));
 app.use(express.json());
 
 await connectRedis();
 connectDB();
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.send('server sun raha hai')
 })
-app.use('/api/auth',authRoutes);
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>console.log(`server is running at port ${PORT}`));
+app.listen(PORT, () => console.log(`server is running at port ${PORT}`));

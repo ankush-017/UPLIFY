7403d58a49c59844f24c78067450e401c680eb8b
Ankush kumar <ankushkumar1412005@gmail.com>
2025-06-25 22:16:27 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,22 @@ import { connectRedis } from './Utils/redisClient.js';
 dotenv.config();
 
 const app = express();
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://uplify-alpha.vercel.app'
+];
+
 app.use(cors({
-    origin: "https://uplify.onrender.com",
-    credentials: true
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps, curl, etc.)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
 }));
 app.use(express.json());
 
